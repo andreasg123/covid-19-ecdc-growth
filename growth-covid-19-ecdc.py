122e136c6d7eb28eaf8aa859aa133d081dd9850e
Andreas Girgensohn <andreasg123@users.noreply.github.com>
2020-03-23 10:49:04 -0700

    Use linear regression to determine the average daily growth

--- growth-covid-19-ecdc.py
+++ growth-covid-19-ecdc.py
@@ -3,7 +3,9 @@
 import csv
 import datetime
 import math
+import numpy as np
 import pandas as pd
+from scipy import stats
 import sys
 
 # Outputs tab-separated growth in COVID-19 cases or deaths sorted by the
@@ -14,7 +16,7 @@ path = sys.argv[1] if len(sys.argv) > 1 else 'https://www.ecdc.europa.eu/sites/d
 df = pd.read_excel(path)
 
 column = 'Cases'
-column = 'Deaths'
+# column = 'Deaths'
 
 # Threshold for including a country
 threshold = 500 if column == 'Cases' else 20
@@ -27,15 +29,21 @@ for country, group in df.groupby('GeoId'):
      df2 = group.iloc[::-1][['DateRep', column]]
      df2['cum'] = df2[column].cumsum()
      # Exclude the Diamond Princess
-     if country != 'JPG11668' and len(df2) >= ndays and df2.iloc[-1]['cum'] >= threshold:
-          header = ['ID'] + [t.strftime('%m/%d') for t in df2.iloc[-ndays:]['DateRep']] + ['Daily']
-          rows.append([country] + list(df2.iloc[-ndays:]['cum']) +
-                      [(math.pow(df2.iloc[-1]['cum'] / df2.iloc[-ndays]['cum'],
-                                 1 / (ndays - 1)) - 1) * 100])
+     if country == 'JPG11668':
+          continue
+     if len(df2) < ndays or df2.iloc[-ndays]['cum'] == 0 or df2.iloc[-1]['cum'] < threshold:
+          continue
+     header = ['ID'] + [t.strftime('%m/%d') for t in df2.iloc[-ndays:]['DateRep']] + ['Daily']
+     x = np.arange(ndays)
+     y = np.log(df2.iloc[-ndays:]['cum'])
+     slope, _, _, _, _ = stats.linregress(x, y)
+     rows.append([country] + list(df2.iloc[-ndays:]['cum']) + [math.exp(slope)])
+
+# print(rows)
 
 rows.sort(reverse=True, key=lambda x: x[ndays + 1])
 writer = csv.writer(sys.stdout, delimiter='\t')
 writer.writerow(header)
 for r in rows:
-     r[ndays + 1] = '%.0f%%' % r[ndays + 1]
+     r[ndays + 1] = '%.0f%%' % (100 * (r[ndays + 1] - 1))
      writer.writerow(r)
